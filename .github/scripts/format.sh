51248992c125ac518573ef2b42effd1af2315d07
bernard-wagner <bernard-wagner@users.noreply.github.com>
2025-08-14 08:00:20 +0200

    ci: pin to rust 1.88.0 to fix for undefined reference to

--- .github/scripts/format.sh
+++ .github/scripts/format.sh
@@ -3,5 +3,5 @@ set -eo pipefail
 
 # We have to ignore at shell level because testdata/ is not a valid Foundry project,
 # so running `forge fmt` with `--root testdata` won't actually check anything
-cargo run --bin forge -- fmt "$@" \
+cargo run --bin arbos-forge -- fmt "$@" \
     $(find testdata -name '*.sol' ! -name Vm.sol ! -name console.sol)
