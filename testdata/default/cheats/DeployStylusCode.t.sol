59e3f8a7aea3bcb5607c75e52f36fbb5121c2b8a
bernard-wagner <bernard-wagner@users.noreply.github.com>
2025-08-23 16:06:26 +0200

    case sensitive paths

--- testdata/default/cheats/DeployStylusCode.t.sol
+++ testdata/default/cheats/DeployStylusCode.t.sol
@@ -17,46 +17,46 @@ contract DeployStylusCodeTest is DSTest {
 
     function testStylusDeployCode() public {
         bytes memory defaultWasm =
-            abi.encodePacked(hex"eff00000", vm.readFileBinary("fixtures/stylus/foundry_stylus_program.wasm"));
-        address addrDeployCode = vm.deployStylusCode("fixtures/stylus/foundry_stylus_program.wasm");
+            abi.encodePacked(hex"eff00000", vm.readFileBinary("fixtures/Stylus/foundry_stylus_program.wasm"));
+        address addrDeployCode = vm.deployStylusCode("fixtures/Stylus/foundry_stylus_program.wasm");
 
         assertEq(defaultWasm, addrDeployCode.code);
     }
 
     function testStylusDeployCodeWithArgs() public {
         bytes memory defaultWasm = abi.encodePacked(
-            hex"eff00000", vm.readFileBinary("fixtures/stylus/foundry_stylus_program_constructor.wasm")
+            hex"eff00000", vm.readFileBinary("fixtures/Stylus/foundry_stylus_program_constructor.wasm")
         );
         address addrDeployCode =
-            vm.deployStylusCode("fixtures/stylus/foundry_stylus_program_constructor.wasm", abi.encode(1337));
+            vm.deployStylusCode("fixtures/Stylus/foundry_stylus_program_constructor.wasm", abi.encode(1337));
         assertEq(defaultWasm, addrDeployCode.code);
         assertEq(TestContract(addrDeployCode).get_constructor_value(), 1337);
     }
 
     function testStylusDeployCodeWithPayableConstructor() public {
         bytes memory defaultWasm = abi.encodePacked(
-            hex"eff00000", vm.readFileBinary("fixtures/stylus/foundry_stylus_program_payable_constructor.wasm")
+            hex"eff00000", vm.readFileBinary("fixtures/Stylus/foundry_stylus_program_payable_constructor.wasm")
         );
         address addrDeployCode =
-            vm.deployStylusCode("fixtures/stylus/foundry_stylus_program_payable_constructor.wasm", abi.encode(1337));
+            vm.deployStylusCode("fixtures/Stylus/foundry_stylus_program_payable_constructor.wasm", abi.encode(1337));
         assertEq(defaultWasm, addrDeployCode.code);
         assertEq(TestContract(addrDeployCode).get_constructor_value(), 1337);
     }
 
     function testStylusDeployCodeWithSalt() public {
         bytes memory defaultWasm =
-            abi.encodePacked(hex"eff00000", vm.readFileBinary("fixtures/stylus/foundry_stylus_program.wasm"));
-        address addrDeployCode = vm.deployStylusCode("fixtures/stylus/foundry_stylus_program.wasm", bytes32("salt"));
+            abi.encodePacked(hex"eff00000", vm.readFileBinary("fixtures/Stylus/foundry_stylus_program.wasm"));
+        address addrDeployCode = vm.deployStylusCode("fixtures/Stylus/foundry_stylus_program.wasm", bytes32("salt"));
 
         assertEq(defaultWasm, addrDeployCode.code);
     }
 
     function testStylusDeployCodeWithArgsAndSalt() public {
         bytes memory defaultWasm = abi.encodePacked(
-            hex"eff00000", vm.readFileBinary("fixtures/stylus/foundry_stylus_program_constructor.wasm")
+            hex"eff00000", vm.readFileBinary("fixtures/Stylus/foundry_stylus_program_constructor.wasm")
         );
         address addrDeployCode = vm.deployStylusCode(
-            "fixtures/stylus/foundry_stylus_program_constructor.wasm", abi.encode(1337), bytes32("salt")
+            "fixtures/Stylus/foundry_stylus_program_constructor.wasm", abi.encode(1337), bytes32("salt")
         );
         assertEq(defaultWasm, addrDeployCode.code);
         assertEq(TestContract(addrDeployCode).get_constructor_value(), 1337);
@@ -64,10 +64,10 @@ contract DeployStylusCodeTest is DSTest {
 
     function testStylusDeployCodeWithPayableConstructorAndSalt() public {
         bytes memory defaultWasm = abi.encodePacked(
-            hex"eff00000", vm.readFileBinary("fixtures/stylus/foundry_stylus_program_payable_constructor.wasm")
+            hex"eff00000", vm.readFileBinary("fixtures/Stylus/foundry_stylus_program_payable_constructor.wasm")
         );
         address addrDeployCode = vm.deployStylusCode(
-            "fixtures/stylus/foundry_stylus_program_payable_constructor.wasm", abi.encode(1337), bytes32("salt")
+            "fixtures/Stylus/foundry_stylus_program_payable_constructor.wasm", abi.encode(1337), bytes32("salt")
         );
         assertEq(defaultWasm, addrDeployCode.code);
         assertEq(TestContract(addrDeployCode).get_constructor_value(), 1337);
@@ -75,10 +75,10 @@ contract DeployStylusCodeTest is DSTest {
 
     function testStylusDeployCodeWithPayableConstructorAndArgsAndSalt() public {
         bytes memory defaultWasm = abi.encodePacked(
-            hex"eff00000", vm.readFileBinary("fixtures/stylus/foundry_stylus_program_payable_constructor.wasm")
+            hex"eff00000", vm.readFileBinary("fixtures/Stylus/foundry_stylus_program_payable_constructor.wasm")
         );
         address addrDeployCode = vm.deployStylusCode(
-            "fixtures/stylus/foundry_stylus_program_payable_constructor.wasm", abi.encode(1337), 101, bytes32("salt")
+            "fixtures/Stylus/foundry_stylus_program_payable_constructor.wasm", abi.encode(1337), 101, bytes32("salt")
         );
         assertEq(defaultWasm, addrDeployCode.code);
         assertEq(TestContract(addrDeployCode).get_constructor_value(), 1337);
