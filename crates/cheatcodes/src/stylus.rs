bfe25faf5013e8d5210ee5c0cd117194dff0afa7
bernard-wagner <bernard-wagner@users.noreply.github.com>
2025-10-06 14:35:02 +0200

    feat(cheatcodes): deployStylusCode strips reference-types

--- crates/cheatcodes/src/stylus.rs
+++ crates/cheatcodes/src/stylus.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::PathBuf};
+use std::{fs, ops::Range, path::PathBuf};
 
 use alloy_primitives::{Bytes, U256, hex};
 use alloy_sol_types::SolValue;
@@ -8,8 +8,12 @@ use revm::{
     interpreter::{CallInputs, CallScheme, CreateInputs},
 };
 use spec::Vm::*;
+use wasm_encoder::{Module, RawSection};
+use wasmparser::{Parser, Payload};
 
 use crate::{Cheatcode, Cheatcodes, CheatcodesExecutor, CheatsCtxt, Result};
+
+// pub const PROJECT_HASH_SECTION_NAME: &str = "project_hash";
 
 impl Cheatcode for deployStylusCode_0Call {
     fn apply_full(&self, ccx: &mut CheatsCtxt, executor: &mut dyn CheatcodesExecutor) -> Result {
@@ -141,43 +145,55 @@ fn deploy_stylus_code(
 /// - `path/to/artifact.wasm`
 /// - `path/to/artifact.wasm.br`
 fn get_artifact_code(state: &Cheatcodes, path: &str, compress: bool) -> Result<Bytes> {
-    let path = if path.ends_with(".wasm") || path.ends_with(".wasm.br") {
+    let path = if path.ends_with(".wasm") {
         PathBuf::from(path)
     } else {
         bail!("invalid artifact path")
     };
 
     let path = state.config.ensure_path_allowed(path, FsAccessKind::Read)?;
-    let artifact = fs::read(path)?;
+    let wasm = fs::read(path)?;
 
-    // if it starts with WASM magic number, we assume it's a Stylus artifact and we first
-    // need to compress it and then prefix it with Stylus discriminant
-    let artifact = if artifact.starts_with(&[0x00, 0x61, 0x73, 0x6d]) && compress {
+
+    // We convert the WASM from binary to text and back to binary as this trick removes any dangling
+    // mentions of reference types in the wasm body, which are not yet supported by Arbitrum chain backends.
+    let wat_str = if let Ok(wat_str) = wasmprinter::print_bytes(&wasm) {
+        wat_str
+    } else {
+        bail!("failed to convert WASM to WAT")
+    };
+
+    let wasm = if let Ok(wasm) = stylus::wasmer::wat2wasm(wat_str.as_bytes()) {
+        wasm
+    } else {
+        bail!("failed to convert WAT to WASM")
+    };
+
+    // let wasm = if let Some(project_hash) = project_hash_section(&wasm) {
+    //     add_custom_section(&wasm, project_hash[0..32].try_into().unwrap())
+    // } else {
+    //    wasm.to_vec()
+    // };
+
+    let wasm = strip_user_metadata(&wasm)?;
+
+    let wasm = if compress {
         // Compress the artifact if it is a Stylus artifact
         if let Ok(compressed) =
-            stylus::brotli::compress(&artifact, 11, 22, stylus::brotli::Dictionary::StylusProgram)
+            stylus::brotli::compress(&wasm, 11, 22, stylus::brotli::Dictionary::Empty)
         {
             compressed
         } else {
             bail!("failed to compress stylus artifact")
         }
     } else {
-        artifact
+        wasm
     };
 
-    // If the artifact is already prefixed with Stylus discriminant, we can return it as is.
-    // Otherwise, we need to prefix it with Stylus discriminant and compression level.
-
-    let artifact = if artifact.starts_with(arbos_revm::constants::STYLUS_DISCRIMINANT) {
-        // If the artifact is already prefixed with Stylus discriminant, we can return it as is.
-        artifact
-    } else {
-        // If not, we need to prefix it with Stylus discriminant and compression level.
-        [arbos_revm::constants::STYLUS_DISCRIMINANT, &[compress as u8], artifact.as_ref()].concat()
-    };
+    let wasm = [arbos_revm::constants::STYLUS_DISCRIMINANT, &[arbos_revm::constants::STYLUS_EOF_NO_DICT], wasm.as_ref()].concat();
 
     // add init code
-    let artifact = get_init_code_of_empty_constructor(artifact);
+    let artifact = get_init_code_of_empty_constructor(wasm);
 
     Ok(Bytes::from(artifact))
 }
@@ -206,4 +222,60 @@ fn get_init_code_of_empty_constructor(bytecode: Vec<u8>) -> Vec<u8> {
     init.extend_from_slice(&bytecode);
 
     init
+}
+
+// pub fn project_hash_section(wasm_file_bytes: &[u8]) -> Option<&[u8]> {
+//     let parser = wasmparser::Parser::new(0);
+//     for payload in parser.parse_all(wasm_file_bytes) {
+//         if let Ok(payload) = payload {
+//             if let wasmparser::Payload::CustomSection(reader) = payload {
+//                 if reader.name() == PROJECT_HASH_SECTION_NAME {
+//                     return Some(reader.data());
+//                 }
+//             }
+//         }
+//     }
+//     None
+// }
+
+// fn add_custom_section(wasm_file_bytes: &[u8], project_hash: [u8; 32]) -> Vec<u8> {
+//     let mut bytes = vec![];
+//     bytes.extend_from_slice(wasm_file_bytes);
+//     wasm_gen::write_custom_section(&mut bytes, PROJECT_HASH_SECTION_NAME, &project_hash);
+//     bytes
+// }
+
+fn strip_user_metadata(wasm_file_bytes: &[u8]) -> Result<Vec<u8>> {
+    let mut module = Module::new();
+    // Parse the input WASM and iterate over the sections
+    let parser = Parser::new(0);
+    for payload in parser.parse_all(wasm_file_bytes) {
+        match payload {
+            Ok(Payload::CustomSection { .. }) => {
+                // Skip custom sections to remove sensitive metadata
+                debug!("stripped custom section from user wasm to remove any sensitive data");
+            }
+            Ok(Payload::UnknownSection { .. }) => {
+                // Skip unknown sections that might not be sensitive
+                debug!("stripped unknown section from user wasm to remove any sensitive data");
+            }
+            Ok(item) => {
+                // Handle other sections as normal.
+                if let Some(section) = item.as_section() {
+                    let (id, range): (u8, Range<usize>) = section;
+                    let data_slice = &wasm_file_bytes[range.start..range.end]; // Start at the beginning of the range
+                    let raw_section = RawSection {
+                        id,
+                        data: data_slice,
+                    };
+                    module.section(&raw_section);
+                }
+            },
+            Err(e) => {
+                bail!("error parsing wasm: {}", e);
+            }
+        }
+    }
+    // Return the stripped WASM binary
+    Ok(module.finish())
 }
