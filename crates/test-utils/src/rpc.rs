5a426d2005c2a85e097cd50608fba42c84ddfad6
bernard-wagner <bernard-wagner@users.noreply.github.com>
2026-09-06 18:48:18 +0000

    test: replace retired Moonbeam RPC

--- crates/test-utils/src/rpc.rs
+++ crates/test-utils/src/rpc.rs
@@ -100,7 +100,7 @@ pub fn rpc_endpoints() -> RpcEndpoints {
         ("polygon", RpcEndpointUrl::Url(next_rpc_endpoint(NamedChain::Polygon))),
         ("bsc", RpcEndpointUrl::Url(next_rpc_endpoint(NamedChain::BinanceSmartChain))),
         ("avaxTestnet", RpcEndpointUrl::Url("https://api.avax-test.network/ext/bc/C/rpc".into())),
-        ("moonbeam", RpcEndpointUrl::Url("https://moonbeam-rpc.publicnode.com".into())),
+        ("moonbeam", RpcEndpointUrl::Url("https://moonbeam.api.onfinality.io/public".into())),
         ("rpcEnvAlias", RpcEndpointUrl::Env("${RPC_ENV_ALIAS}".into())),
     ])
 }
